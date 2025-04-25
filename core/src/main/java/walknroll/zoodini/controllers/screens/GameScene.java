b7e7ddf277035d99303ed501d6c361d9a852ef52
billpark1234 <bill.park.cs@gmail.com>
2025-04-24 20:59:17 -0400

    Ink meter animating properly. Chatgpt is better than me bro

--- core/src/main/java/walknroll/zoodini/controllers/screens/GameScene.java
+++ core/src/main/java/walknroll/zoodini/controllers/screens/GameScene.java
@@ -64,7 +64,7 @@ import walknroll.zoodini.utils.ZoodiniSprite;
  */
 public class GameScene implements Screen, ContactListener, UIController.PauseMenuListener {
 
-    private boolean debug = true;
+    private boolean debug = false;
 
 	// ASSETS
 	/** Need an ongoing reference to the asset directory */
