11c72d34cda4679f851de1eefe3a568320841027
Nick Regennitter <nickregennitter12@gmail.com>
2025-04-24 21:11:19 -0400

    refactor and deduplicate some UI code

--- core/src/main/java/walknroll/zoodini/controllers/screens/GameWinScene.java
+++ core/src/main/java/walknroll/zoodini/controllers/screens/GameWinScene.java
@@ -37,6 +37,7 @@ import edu.cornell.gdiac.assets.AssetDirectory;
 import edu.cornell.gdiac.graphics.SpriteBatch;
 import edu.cornell.gdiac.util.ScreenListener;
 import walknroll.zoodini.GDXRoot;
+import walknroll.zoodini.utils.MenuButton;
 
 /**
  * Class that provides a loading screen for the state of the game.
@@ -46,48 +47,6 @@ import walknroll.zoodini.GDXRoot;
  * by a play button. You are free to adopt this to your needs.
  */
 public class GameWinScene implements Screen, InputProcessor {
-    /** Wrapper class for instantiating menu buttons */
-    private class MenuButton {
-        public float x;
-        public float y;
-        public float width;
-        public float height;
-
-        private int pressedState;
-        private String assetName;
-        private boolean pressed;
-
-        public MenuButton(float x, float y, float width, float height, String assetName, int pressedState) {
-            this.x = x;
-            this.y = y;
-            this.width = width;
-            this.height = height;
-            this.assetName = assetName;
-            this.pressedState = pressedState;
-            this.pressed = false;
-        }
-
-        public boolean contains(float x, float y) {
-            return (x >= this.x) && (y >= this.y) && (x <= this.x + this.width) && (y <= this.y + this.height);
-        }
-
-        public String getAssetName() {
-            return this.assetName;
-        }
-
-        public int getPressedState() {
-            return this.pressedState;
-        }
-
-        public void press() {
-            this.pressed = !this.pressed;
-        }
-
-        public boolean isPressed() {
-            return this.pressed;
-        }
-    }
-
     // There are TWO asset managers.
     // One to load the loading screen. The other to load the assets
     /** The actual assets to be loaded */
@@ -109,7 +68,6 @@ public class GameWinScene implements Screen, InputProcessor {
 
     /** The constants for arranging images on the screen */
     JsonValue constants;
-    private float scale;
 
     /** The current state of the play button */
     private Integer pressState;
@@ -205,9 +163,6 @@ public class GameWinScene implements Screen, InputProcessor {
      * @param height The new height in pixels
      */
     public void resize(int width, int height) {
-        // Compute the drawing scale
-        scale = ((float) height) / constants.getFloat("height");
-
         this.width = width;
         this.height = height;
         if (camera == null) {
