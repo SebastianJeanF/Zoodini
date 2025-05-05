60c35a309f16a93124416af401151cda7fb2ec72
Sebastian Jean-Francois <sebeld9@gmail.com>
2025-05-04 22:13:35 -0400

    Add tutorial text to level 1

---
+++ assets/tilesets/decorations.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="decorations" tilewidth="128" tileheight="128" tilecount="120" columns="12">
+ <image source="decorations.png" width="1566" height="1283"/>
+</tileset>
