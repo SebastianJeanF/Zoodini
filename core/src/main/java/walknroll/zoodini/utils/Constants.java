acd4fb82f2d29ce38e800b193fb9da6c15b50d27
billpark1234 <bill.park.cs@gmail.com>
2025-05-04 20:57:58 -0400

    Flip camera sprite left or right based on angle.

--- core/src/main/java/walknroll/zoodini/utils/Constants.java
+++ core/src/main/java/walknroll/zoodini/utils/Constants.java
@@ -3,6 +3,6 @@ package walknroll.zoodini.utils;
 public class Constants {
     public static final float CAMERA_INTERPOLATION_DURATION = 0.75f;
     public static final boolean DEBUG = false; // Enables debug printing, debug drawing, and unlocks all levels
-    public static final boolean UNLOCK_ALL = false;
+    public static final boolean UNLOCK_ALL = true;
     public static final boolean INVINCIBLE = false;
 }
