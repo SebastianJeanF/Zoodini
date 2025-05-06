b3aaa8912f87c24ccf1bd720139e111fe3012ace
Andrew Cheung <andrewcheung360@gmail.com>
2025-05-06 00:54:02 -0400

    nerfed level

--- core/src/main/java/walknroll/zoodini/utils/Constants.java
+++ core/src/main/java/walknroll/zoodini/utils/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
     public static final float CAMERA_INTERPOLATION_DURATION = 0.75f;
     public static final boolean DEBUG = true;
     public static final boolean UNLOCK_ALL = true;
-    public static final boolean INVINCIBLE = true;
+    public static final boolean INVINCIBLE = false;
 }
