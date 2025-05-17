be4cc7c4e6e95eb27602f9787b02e1b6eb96ae71
Nick Regennitter <nickregennitter12@gmail.com>
2025-05-17 01:09:28 -0400

    add new path tiles

---
+++ assets/tilesets/path-4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="path-4" tilewidth="128" tileheight="128" tilecount="88" columns="11">
+ <image source="path-4.png" width="1513" height="1024"/>
+</tileset>
